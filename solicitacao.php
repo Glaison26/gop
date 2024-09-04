0673b6384474eee14126a2d6b42eb129491ca37e
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-09-04 13:29:06 -0300

    dev

---
+++ solicitacao.php
@@ -0,0 +1,48 @@
+<?php // controle de acesso ao formulário
+session_start();
+if (!isset($_SESSION['newsession'])) {
+    die('Acesso não autorizado!!!');
+}
+include("conexao.php");
+include("links2.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <div class="panel panel-primary class">
+        <div class="panel-heading text-center">
+            <h4>GOP - Gestão Operacional</h4>
+            <h5>Solicitações de Serviços<h5>
+        </div>
+    </div>
+
+    <div class="container -my5">
+        <div class='alert alert-info' role='alert'>
+            <div style="padding-left:15px;">
+                <img Align="left" src="\gop\images\escrita.png" alt="30" height="35">
+
+            </div>
+            <h5>Clique em nova solitação para abrir uma nova solicitação de serviço ou realize uma pesquisa com as opções abaixo</h5>
+        </div>
+        <a class="btn btn-success btn-sm" href="solicitacao_nova.php"><img src="\gop\images\contato.png" alt="" width="25" height="25"> Nova Solicitação</a>
+        <a class="btn btn-info btn-sm" href="#"><img src="\gop\images\lupa.png" alt="" width="25" height="25"> Pesquisar</a>
+        <a class="btn btn-warning btn-sm" href="#"><img src="\gop\images\eraser.png" alt="" width="25" height="25"> Limpar pesquisa</a>
+        <a class="btn btn-secondary btn-sm" href="\gop\menu.php"><img src="\gop\images\voltar.png" alt="" width="25" height="25"> Voltar</a>
+        <hr>
+    <p><h5>Opções para pesquisa</h5></p>
+    </div>
+    
+   
+
+</body>
+
+</html>
---
+++ solicitacao_nova.php
@@ -0,0 +1,88 @@
+<?php // controle de acesso ao formulário
+session_start();
+if (!isset($_SESSION['newsession'])) {
+    die('Acesso não autorizado!!!');
+}
+include("conexao.php");
+include("links2.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <div class="panel panel-primary class">
+        <div class="panel-heading text-center">
+            <h4>GOP - Gestão Operacional</h4>
+            <h5>Solicitações de Serviços<h5>
+        </div>
+    </div>
+
+    <div class="container -my5">
+
+        <div class='alert alert-info' role='alert'>
+            <div style="padding-left:15px;">
+                <img Align="left" src="\gop\images\escrita.png" alt="30" height="35">
+
+            </div>
+            <h5>Escolha o tipo da solicitação entre Recurso Fisico, Espaço Físico ou solicitação avulsa</h5>
+        </div>
+
+
+
+        <body class="sb-nav-fixed">
+            <main>
+                <hr>
+
+                <div class="row mx-auto h-25" style="max-width: 750px;">
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card  mb-6">
+                            <img src="\gop\images\config.png" height="140px" class="card-img-top" alt="...">
+                            <div class="card-body">Serviço em Recurso Físico</div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="#" class="btn btn-primary">Clique aqui</a>
+                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card mb-6">
+                            <img src="\gop\images\apartamento.png" height="140px" class="card-img-top" alt="...">
+                            <div class="card-body">Serviço em Espaço Físico</div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="#"  class="btn btn-primary">Clique aqui</a>
+                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card mb-6">
+                            <img src="\gop\images\ordem.png" height="140px" class="card-img-top" alt="...">
+                            <div class="card-body">Solicitação de Serviço Avulsa</div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="#"  class="btn btn-primary">Clique aqui</a>
+                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </main>
+        </body>
+
+    </div>
+   
+
+</body>
+
+
+
+</html>
