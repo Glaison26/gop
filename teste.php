6a5b98b85e62f4c51c6b1812e7d5e2d9f27c1cd5
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-09-12 21:07:57 -0300

    dev email

--- teste.php
+++ teste.php
@@ -1,11 +1,41 @@
 <?php
-// the message
-$msg = "First line of text";
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-// send email
-mail("glaison26.queiroz@gmail.com","My subject",$msg);
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'glaison26.queiroz@gmail.com';                     //SMTP username
+    $mail->Password   = 'ckcf ndns wvoa kehu';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('glaison26.queiroz@gmail.com', 'GOP');
+    $mail->addAddress('glaison26.queiroz@gmail.com', 'Glaison');     // endereco para onde será enviado
+   
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Confirmação de Solicitação de Serviço GOP';
+    $mail->Body    = 'Sua solicitação de Serviço foi feita com sucesso, favor aguardar o atendimento';
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 ?>
