4228f6135e3e1e836208cb22d5c6eee1fa9a838b
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-09-30 16:30:34 -0300

    dev

--- preventivas_gerar.php
+++ preventivas_gerar.php
@@ -23,7 +23,7 @@ JOIN recursos ON preventivas.id_recurso=recursos.id
 where data_prox_realizacao<='$agora' and gerar = 'Sim'
 ORDER BY preventivas.data_prox_realizacao desc";
 // sql com as preventiva com data da próxima realização igual ou inferior a data do dia em espaço fisico
-$c_sql_espaco = "SELECT preventivas.id, preventivas.periodicidade_geracao, preventivas.calibracao, preventivas.id_espaco, espacos.descricao as espaco,
+$c_sql_espaco = "SELECT preventivas.id, preventivas.tipo, preventivas.periodicidade_geracao, preventivas.calibracao, preventivas.id_espaco, espacos.descricao as espaco,
 preventivas.data_ult_realizacao, preventivas.data_prox_realizacao, preventivas.gerar, preventivas.id_ocorrencia,
 case
 when preventivas.calibracao ='S' then 'Sim'
@@ -51,20 +51,19 @@ $c_sql_responsavel = "Select id from usuarios where login='$c_responsavel'";
 $result_responsavel = $conection->query($c_sql_responsavel);
 $registro_responsavel = $result_responsavel->fetch_assoc();
 $i_id_solicitante = $registro_responsavel['id'];
-
 // variaveis para fazer o insert
 $i_id_recurso = $c_linha['id_recurso'];
 $i_id_oficina = $c_linha['id_oficina'];
 //
 $d_data_geracao =  date("Y-m-d");
 $d_hora_geracao = date('H:i');
-$c_tipo = 'R'; // recurso fisico, espaço fisico ou avulsa 
+$c_tipo = $c_linha['tipo']; // recurso fisico, espaço fisico ou avulsa 
 $c_tipo_ordem = 'P'; // Preventiva 
 $c_descritivo = $c_linha['descritivo'];
-
+$c_tipo_preventiva = $c_linha['tipo_preventiva'];
 // data de inicio
 $d_data_inicio = date("Y-m-d");
 $d_hora_inicio = date('H:i');
 $i_id_ocorrencia = $result['id_ocorrencia'];
-//
+// 
 ?>
