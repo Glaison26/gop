6c2008f2f2b15d877f9eadb4a5591332a3200c5c
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-10-03 11:33:15 -0300

    dev

--- banco/gopbanco.sql
+++ banco/gopbanco.sql
@@ -19,6 +19,22 @@
 CREATE DATABASE IF NOT EXISTS `gop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `gop`;
 
+-- Copiando estrutura para tabela gop.preventivas_checklist
+CREATE TABLE IF NOT EXISTS `preventivas_checklist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_check` int DEFAULT NULL,
+  `id_preventiva` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_preventivas_checklist_checklist` (`id_check`),
+  KEY `FK_preventivas_checklist_preventivas` (`id_preventiva`),
+  CONSTRAINT `FK_preventivas_checklist_checklist` FOREIGN KEY (`id_check`) REFERENCES `checklist` (`id`),
+  CONSTRAINT `FK_preventivas_checklist_preventivas` FOREIGN KEY (`id_preventiva`) REFERENCES `preventivas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela gop.preventivas_checklist: ~0 rows (aproximadamente)
+REPLACE INTO `preventivas_checklist` (`id`, `id_check`, `id_preventiva`) VALUES
+	(2, 1, 8);
+
 -- Copiando estrutura para tabela gop.preventivas_executores
 CREATE TABLE IF NOT EXISTS `preventivas_executores` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -29,12 +45,30 @@ CREATE TABLE IF NOT EXISTS `preventivas_executores` (
   KEY `FK_preventivas_executores_executores` (`id_executor`) USING BTREE,
   CONSTRAINT `FK_preventivas_executores_executores` FOREIGN KEY (`id_executor`) REFERENCES `executores` (`id`),
   CONSTRAINT `FK_preventivas_executores_preventivas` FOREIGN KEY (`id_preventiva`) REFERENCES `preventivas` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Copiando dados para a tabela gop.preventivas_executores: ~2 rows (aproximadamente)
 REPLACE INTO `preventivas_executores` (`id`, `id_preventiva`, `id_executor`) VALUES
 	(12, 4, 6),
-	(13, 4, 5);
+	(13, 4, 5),
+	(14, 8, 5),
+	(15, 8, 7);
+
+-- Copiando estrutura para tabela gop.preventivas_ferramentas
+CREATE TABLE IF NOT EXISTS `preventivas_ferramentas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_ferramenta` int DEFAULT NULL,
+  `id_preventiva` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__ferramentas` (`id_ferramenta`),
+  KEY `FK__preventivas` (`id_preventiva`),
+  CONSTRAINT `FK__ferramentas` FOREIGN KEY (`id_ferramenta`) REFERENCES `ferramentas` (`id`),
+  CONSTRAINT `FK__preventivas` FOREIGN KEY (`id_preventiva`) REFERENCES `preventivas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela gop.preventivas_ferramentas: ~0 rows (aproximadamente)
+REPLACE INTO `preventivas_ferramentas` (`id`, `id_ferramenta`, `id_preventiva`) VALUES
+	(1, 6, 8);
 
 -- Copiando estrutura para tabela gop.preventivas_materiais
 CREATE TABLE IF NOT EXISTS `preventivas_materiais` (
@@ -50,11 +84,29 @@ CREATE TABLE IF NOT EXISTS `preventivas_materiais` (
   CONSTRAINT `FK_preventivas_materiais_materiais` FOREIGN KEY (`id_material`) REFERENCES `materiais` (`id`),
   CONSTRAINT `FK_preventivas_materiais_preventivas` FOREIGN KEY (`id_preventiva`) REFERENCES `preventivas` (`id`),
   CONSTRAINT `FK_preventivas_materiais_unidades` FOREIGN KEY (`id_unidade`) REFERENCES `unidades` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Copiando dados para a tabela gop.preventivas_materiais: ~0 rows (aproximadamente)
 REPLACE INTO `preventivas_materiais` (`id`, `id_preventiva`, `id_material`, `id_unidade`, `quantidade`) VALUES
-	(16, 4, 2, 8, 15);
+	(16, 4, 2, 8, 15),
+	(17, 8, 4, 10, 5),
+	(18, 8, 2, 8, 120);
+
+-- Copiando estrutura para tabela gop.preventivas_pop
+CREATE TABLE IF NOT EXISTS `preventivas_pop` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_pop` int DEFAULT NULL,
+  `id_preventiva` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_preventivas_pop_pops` (`id_pop`),
+  KEY `FK_preventivas_pop_preventivas` (`id_preventiva`),
+  CONSTRAINT `FK_preventivas_pop_pops` FOREIGN KEY (`id_pop`) REFERENCES `pops` (`id`),
+  CONSTRAINT `FK_preventivas_pop_preventivas` FOREIGN KEY (`id_preventiva`) REFERENCES `preventivas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela gop.preventivas_pop: ~0 rows (aproximadamente)
+REPLACE INTO `preventivas_pop` (`id`, `id_pop`, `id_preventiva`) VALUES
+	(1, 2, 8);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
