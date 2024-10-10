30ed7dc32b83037ac8ea22abef942a3ae5c69d01
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-10-10 12:06:52 -0300

    dev

--- plano_acao/planejamento_estrategia.php
+++ plano_acao/planejamento_estrategia.php
@@ -21,19 +21,30 @@ $_SESSION['volta_estrategia'] = 'S';
 $c_sql = "Select * from planejamento where id='$i_id'";
 $result = $conection->query($c_sql);
 $c_nome = $result->fetch_assoc();
-$c_planejamento = $c_nome['descricao'];
+$c_planejamento = $c_nome['descritivo'];
+
+// rotina para incluir diretrizes da estratégia selecionada ao palenjamento
+if ((isset($_POST["btnincluir"])) && ($_SERVER['REQUEST_METHOD'] == 'POST')) {
+    $c_info_estrategia = $_POST['estr'];
+    // sql para selecioanr estrategia selecionada
+    $c_sql_estr = "select estrategias.id from estrategias where estrategias.descricao='$c_info_estrategia'";
+    $result = $conection->query($c_sql_estr);
+    $c_estr = $result->fetch_assoc();
+    $i_estrategia = $c_estr['id'];
+    // pego diretrizes anexos a estrategia selecionada
+    $c_sql_anexos = "select diretriz_estrategia.id, diretriz_estrategia.id_diretriz, diretriz_estrategia.id_estrategia from diretriz_estrategia where diretriz_estrategia.id_estrategia='$i_estrategia'";
+    //echo $c_sql_anexos;
+    $result = $conection->query($c_sql_anexos);
+    while ($c_linha = $result->fetch_assoc()) {
+        // insiro os anexos e estartegia selecionada
+        $id_dir= $c_linha['id_diretriz'];
+        $id_estr = $c_linha['id_estrategia'];
+        $c_sql_ins = "insert into planejamento_diretrizes (id_planejamento, id_diretriz, id_estrategia) value ('$i_id', '$id_dir', '$id_estr')";
+        $result_ins = $conection->query($c_sql_ins);
+    }
+}
 
 // faço a Leitura da tabela com sql
-$c_sql = "SELECT diretriz_estrategia.id, estrategias.descricao as estrategia, diretrizes.descricao as diretriz FROM diretriz_estrategia
-          JOIN estrategias ON diretriz_estrategia.id_estrategia=estrategias.id
-          JOIN diretrizes ON diretriz_estrategia.id_diretriz=diretrizes.id
-          where id_estrategia='$i_id'
-          ORDER BY diretrizes.descricao";
-$result = $conection->query($c_sql);
-// verifico se a query foi correto
-if (!$result) {
-    die("Erro ao Executar Sql!!" . $conection->connect_error);
-}
 
 ?>
 <!doctype html>
@@ -45,7 +56,7 @@ if (!$result) {
         function confirmacao(id) {
             var resposta = confirm("Deseja remover esse registro?");
             if (resposta == true) {
-                window.location.href = "/gop/plano_acao/estrategia_diretriz_excluir.php?id=" + id;
+                window.location.href = "/gop/plano_acao/planejamento_estrategia_excluir.php?id=" + id;
             }
         }
     </script>
@@ -65,7 +76,7 @@ if (!$result) {
                 "order": [1, 'asc'],
                 "aoColumnDefs": [{
                     'bSortable': false,
-                    'aTargets': [2]
+                    'aTargets': [8]
                 }, {
                     'aTargets': [0],
                     "visible": false
@@ -115,7 +126,7 @@ if (!$result) {
         <div class='alert alert-info' role='alert'>
 
             <img Align="left" src="\gop\images\escrita.png" alt="30" height="35">
-            <h4>Diretrizes para - <?php echo $c_estrategia ?></h4>
+            <h4>Diretrizes para - <?php echo $c_planejamento ?></h4>
         </div>
     </div>
     <?php
@@ -138,26 +149,26 @@ if (!$result) {
                     <div class="panel-heading">
 
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">Selecionar Diretriz para Estratégia</label>
+                            <label class="col-sm-2 col-form-label">Selecionar Estratégia para o planejamento</label>
                             <div class="col-sm-5">
-                                <select class="form-select form-select-lg mb-3" id="dir" name="dir">
+                                <select class="form-select form-select-lg mb-3" id="estr" name="estr">
                                     <?php
-                                    // select da tabela de diretrizez
-                                    $c_sql_diretriz = "SELECT diretrizes.id, diretrizes.descricao FROM diretrizes ORDER BY diretrizes.descricao";
-                                    $result_diretriz = $conection->query($c_sql_diretriz);
-                                    while ($c_linha = $result_diretriz->fetch_assoc()) {
+                                    // select da tabela de estratégias
+                                    $c_sql_estrategia = "SELECT estrategias.id, estrategias.descricao FROM estrategias ORDER BY estrategias.descricao";
+                                    $result_estrategia = $conection->query($c_sql_estrategia);
+                                    while ($c_linha = $result_estrategia->fetch_assoc()) {
                                         echo "<option>$c_linha[descricao]</option>";
                                     }
                                     ?>
                                 </select>
                             </div>
                         </div>
-                        <button type="submit" name="btnincluir" id="btnincluir" title="Inclusão de nova diretriz a Estratégia" class="btn btn-success btn-sm"
+                        <button type="submit" name="btnincluir" id="btnincluir" title="Inclusão de nova Estratégia para Planejamento" class="btn btn-success btn-sm"
                             data-toggle="modal" data-target="#novoModal">
                             <span class="glyphicon glyphicon-plus"></span>
                             Incluir
                         </button>
-                        <a class="btn btn-info btn-sm" href="/gop/plano_acao/diretriz_lista.php"><img src='\gop\images\diretrizes.png' alt='16' width='16' height='16'> Diretrizes</a>
+                        <a class="btn btn-info btn-sm" href="/gop/plano_acao/estrategias_lista.php"><img src='\gop\images\iconeestrategia.png' alt='16' width='16' height='16'> Estratégias</a>
                         <a class="btn btn-secondary btn-sm" href="/gop/plano_acao/estrategias_lista.php"><span class="glyphicon glyphicon-off"></span> Voltar</a>
                     </div>
                 </div>
@@ -169,18 +180,26 @@ if (!$result) {
             <thead class="thead">
                 <tr>
                     <th scope="col">#</th>
+                    <th scope="col">Estratégia</th>
                     <th scope="col">Diretriz</th>
+                    <th scope="col">Prazo</th>
+                    <th scope="col">Local</th>
+                    <th scope="col">Responsável</th>
+                    <th scope="col">Custo</th>
+                    <th scope="col">Situação</th>
                     <th scope="col">Opções</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
                 // faço a Leitura da tabela com sql
-                $c_sql = "SELECT diretriz_estrategia.id, estrategias.descricao as estrategia, diretrizes.descricao as diretriz FROM diretriz_estrategia
-                        JOIN estrategias ON diretriz_estrategia.id_estrategia=estrategias.id
-                        JOIN diretrizes ON diretriz_estrategia.id_diretriz=diretrizes.id
-                        where id_estrategia='$i_id'
-                        ORDER BY diretrizes.descricao";
+                $c_sql = "SELECT planejamento_diretrizes.id, estrategias.descricao AS estrategia ,diretrizes.descricao AS diretriz, 
+                        planejamento_diretrizes.prazo, planejamento_diretrizes.`local`, planejamento_diretrizes.responsavel, planejamento_diretrizes.custo,
+                        planejamento_diretrizes.situacao FROM planejamento_diretrizes
+                        JOIN estrategias ON planejamento_diretrizes.id_estrategia = estrategias.id
+                        JOIN diretrizes ON planejamento_diretrizes.id_diretriz = diretrizes.id
+                        where id_planejamento='$i_id'
+                        ORDER BY estrategias.descricao";
                 $result = $conection->query($c_sql);
                 // verifico se a query foi correto
                 if (!$result) {
@@ -188,11 +207,20 @@ if (!$result) {
                 }
                 // insiro os registro do banco de dados na tabela 
                 while ($c_linha = $result->fetch_assoc()) {
-
+                    if ($c_linha['situacao'] == 'C')
+                        $c_situacao = "Concluido";
+                    else
+                        $c_situacao = "Aberto";
                     echo "
                     <tr class='info'>
                     <td>$c_linha[id]</td>
+                    <td>$c_linha[estrategia]</td>
                     <td>$c_linha[diretriz]</td>
+                    <td>$c_linha[prazo]</td>
+                    <td>$c_linha[local]</td>
+                    <td>$c_linha[responsavel]</td>
+                    <td>$c_linha[custo]</td>
+                    <td>$c_situacao</td>
                    
                     <td>
                     <a class='btn btn-danger btn-sm' href='javascript:func()'onclick='confirmacao($c_linha[id])'><span class='glyphicon glyphicon-trash'></span> Excluir</a>
---
+++ plano_acao/planejamento_estrategia_excluir.php
@@ -0,0 +1,18 @@
+<?php // controle de acesso ao formulário
+session_start();
+if (!isset($_SESSION['newsession'])) {
+    die('Acesso não autorizado!!!');
+}
+
+if (!isset($_GET["id"])) {
+    header('location: /gop/plano_acao/planejamento_estrategia.php');
+    exit;
+}
+$c_id = "";
+$c_id = $_GET["id"];
+// conexão dom o banco de dados
+include("../conexao.php");
+// Exclusão do registro
+$c_sql = "delete from planejamento_diretrizes where id=$c_id";
+$result = $conection->query($c_sql);
+header('location: /gop/plano_acao/planejamento_estrategia.php');
