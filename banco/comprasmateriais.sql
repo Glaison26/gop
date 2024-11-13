255ac5290fb1ea619c88d355809923dde2e2f566
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-11-13 16:14:18 -0300

    banco

---
+++ banco/comprasmateriais.sql
@@ -0,0 +1,52 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para gop
+CREATE DATABASE IF NOT EXISTS `gop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `gop`;
+
+-- Copiando estrutura para tabela gop.compras_materiais
+CREATE TABLE IF NOT EXISTS `compras_materiais` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_compra` int DEFAULT NULL,
+  `id_material` int DEFAULT NULL,
+  `id_unidade` int DEFAULT NULL,
+  `quantidade` double DEFAULT NULL,
+  `valor_unitario` double DEFAULT NULL,
+  `valor_total` double DEFAULT NULL,
+  `data_entrega` date DEFAULT NULL,
+  `fator_conversao` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__materiais` (`id_material`),
+  KEY `FK__unidades` (`id_unidade`),
+  KEY `FK_compras_materiais_compras` (`id_compra`),
+  CONSTRAINT `FK__materiais` FOREIGN KEY (`id_material`) REFERENCES `materiais` (`id`),
+  CONSTRAINT `FK__unidades` FOREIGN KEY (`id_unidade`) REFERENCES `unidades` (`id`),
+  CONSTRAINT `FK_compras_materiais_compras` FOREIGN KEY (`id_compra`) REFERENCES `compras` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela gop.compras_materiais: ~0 rows (aproximadamente)
+REPLACE INTO `compras_materiais` (`id`, `id_compra`, `id_material`, `id_unidade`, `quantidade`, `valor_unitario`, `valor_total`, `data_entrega`, `fator_conversao`) VALUES
+	(4, 3, 4, 8, 2, 15, 30, NULL, 1),
+	(5, 3, 3, 8, 2, 20, 40, NULL, 1),
+	(6, 3, 2, 8, 10, 17, 170, NULL, 1);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
