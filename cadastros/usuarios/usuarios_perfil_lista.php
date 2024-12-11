6bb7648586f6532e76e15418a969118f97bda711
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-12-10 22:40:39 -0300

    dev

--- cadastros/usuarios/usuarios_perfil_lista.php
+++ cadastros/usuarios/usuarios_perfil_lista.php
@@ -116,7 +116,7 @@ include("../../links.php");
           
                     <td>
                     <a class='btn btn-secondary btn-sm' href='/gop/cadastros/usuarios/usuarios_perfil_editar.php?id=$c_linha[id]'><span class='glyphicon glyphicon-pencil'></span> Editar</a>
-                    <a class='btn btn-danger btn-sm' href='javascript:func()'onclick='confirmacao($c_linha[id])'><span class='glyphicon glyphicon-trash'></span> Excluir</a>
+                    
                     </td>
 
                     </tr>
