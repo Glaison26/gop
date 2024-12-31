64bac58d844af2625f647a8023474371c56a0ca1
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-12-30 23:14:14 -0300

    dev

--- obras/obras_verifica_grupo.php
+++ obras/obras_verifica_grupo.php
@@ -15,11 +15,10 @@ if (!empty($c_grupo)) {
     $c_sql_grupo_sel = "SELECT obras_grupo.id FROM obras_grupo  where obras_grupo.descricao='$c_grupo'"; // sql para pegar a id do grupo selecionado no combobox
     $result_grupo_sel = $conection->query($c_sql_grupo_sel);
     $c_linha_grupo_sel = $result_grupo_sel->fetch_assoc();
-
     $_SESSION['id_grupo_select'] = $c_linha_grupo_sel['id'];
 } else
     $_SESSION['id_grupo_select'] = 0;
-
+// volto para inclusão "I" ou Edição 'E'
 if ($_SESSION['insumo_crud'] = 'I')
     header('location: /gop/obras/obras_insumos_novo.php');
 else
