32f54dfead775e135c65102ede2c9f41d648f3df
Glaison Queiroz <glaison26.queiroz@gmail.com>
2026-03-06 02:57:01 -0300

    swv

--- ordens/ordens_cancelar.php
+++ ordens/ordens_cancelar.php
@@ -57,10 +57,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // rotina de envio de email da conclusão para solicitante, manutenção e oficina
         // pego o email da manutenção
-        $c_sql_config = "select email_manutencao from configuracoes";
+        $c_sql_config = "select email_manutencao, email_envio from configuracoes";
         $result = $conection->query($c_sql_config);
         $c_linha_email = $result->fetch_assoc();
         $c_email_manutencao = $c_linha_email['email_manutencao'];
+        $c_email_envio = $c_linha_email['email_envio'];
+        
         //echo $c_email_oficina;
         // procuro solicitante para enviar e-mail
 
