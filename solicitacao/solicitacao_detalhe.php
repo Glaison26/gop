f8419545f21be7617ff3e68b779f6dcc27aacc1c
Glaison Queiroz <glaison26.queiroz@gmail.com>
2026-05-17 00:14:34 -0300

    dev

--- solicitacao/solicitacao_detalhe.php
+++ solicitacao/solicitacao_detalhe.php
@@ -173,9 +173,7 @@ $total_anexos = $registro_anexos['total_anexos'];
 
             <hr>
             <div class='alert alert-info' role='alert'>
-                <div style="padding-left:15px;">
-                    <img Align="left" src="\gop\images\escrita.png" alt="30" height="35">
-                </div>
+                
                 <h5>Detalhe da Solicitação de Serviço No. <?php echo $registro['id'] ?> </h5>
             </div>
             <?php
